c892ce9353d54d21e5534590505370d3ecbcbe47
J <knowisdom314@gmail.com>
2025-10-09 21:13:05 +0900

    ✨ [feat] #3 review API 구현

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.v1.endpoints import admin, users, exhibitions, artworks
+from app.api.v1.endpoints import admin, users, exhibitions, artworks, reviews
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(admin.router, prefix="/admin", tags=["Admin"])
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(exhibitions.router, prefix="/exhibitions", tags=["Exhibitions"])
 api_router.include_router(artworks.router, prefix="/artworks", tags=["Artworks"])
+api_router.include_router(reviews.router, prefix="/reviews", tags=["Reviews"])
