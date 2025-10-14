2afb8afd00873c7f437ce02c919eeb6cd2e1f935
J <knowisdom314@gmail.com>
2025-10-15 03:03:57 +0900

    ✨ [feature] 목데이터 시딩 코드 작성

--- app/db/session.py
+++ app/db/session.py
@@ -3,11 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
+from dotenv import load_dotenv
+
+# .env 파일 로드
+load_dotenv()
+
 # 환경변수에서 DATABASE_URL 가져오기
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://lastdance:2025-c6-m15-lastdance-kbejyj@db:5432/lastdance"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # SQLAlchemy 엔진 생성
 engine = create_engine(DATABASE_URL)
